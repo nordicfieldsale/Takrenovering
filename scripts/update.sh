1bf04e811fb5aef004f682f89335c1622b807f4a
nordicfieldsale <info@nordicfieldsale.se>
2026-07-24 16:56:27 +0200

    Delete scripts directory

--- scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Hämtar senaste koden och startar om utan driftstopp värt namnet.
-set -e
-cd "$(dirname "$0")/.."
-./scripts/backup.sh || true
-docker compose build
-docker compose up -d
-docker image prune -f
-echo "Uppdaterat."
